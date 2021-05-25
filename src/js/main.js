d1afc3ba70566b507c8ca628e31e6e9315577b2d
canosaer <canosaer@gmail.com>
2021-05-25 13:53:58 -0400

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -5,8 +5,29 @@ class PosterAnimation {
     }
 
     animate = () => {
+        let sky = document.querySelector('.sky')
+        let city = document.querySelector('.city')
+        let cityTouchup = document.querySelector('.city__touch-up')
+        let yellowSun = document.querySelector('.yellow-sun')
+        let redSun = document.querySelector('.red-sun')
+        let falcon = document.querySelector('.falcon')
+        let ground = document.querySelector('.ground')
+        let groundTouchup1 = document.querySelector('.ground__touch-up')
+        let groundTouchup2 = document.querySelector('.ground__touch-up2')
+        let groundTouchup3 = document.querySelector('.ground__touch-up3')
+        let r2 = document.querySelector('.r2')
 
-        console.log('hi')
+        let r2Timeline = gsap.timeline()
+
+        r2Timeline.to(
+            r2, {
+                left: '300px',
+                top: '300px',
+                duration: 1,
+                ease: 'expo.in',
+            }
+        )
+        
     }
 
 
