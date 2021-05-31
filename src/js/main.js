b99f024e94f91a98310405d1294df662b01440c8
canosaer <canosaer@gmail.com>
2021-05-31 15:03:11 -0400

    1.0

--- src/js/main.js
+++ src/js/main.js
@@ -85,16 +85,134 @@ class PosterAnimation {
                 repeatDelay: 22,
             }
         )
-        // for(let i=0;i<letters.length;i++){
-        //     if(i===0){
-        //         gsap.to(
-        //             letters[i], {
-        //                 duration: 7.5,
-        //                 left: '21%'
-        //             }
-        //         )
-        //     }
-        // }
+        for(let i=0;i<letters.length;i++){
+            if(i===0){
+                gsap.to(
+                    letters[i], {
+                        duration: 5,
+                        ease: 'sine.inOut',
+                        left: '19%'
+                    }
+                )
+            }
+            if(i===1){
+                gsap.to(
+                    letters[i], {
+                        duration: 5,
+                        ease: 'sine.inOut',
+                        left: '23%'
+                    }
+                )
+            }
+            if(i===2){
+                gsap.to(
+                    letters[i], {
+                        duration: 5,
+                        ease: 'sine.inOut',
+                        left: '27%'
+                    }
+                )
+            }
+            if(i===3){
+                gsap.to(
+                    letters[i], {
+                        duration: 5,
+                        ease: 'sine.inOut',
+                        left: '32.4%'
+                    }
+                )
+            }
+            if(i===4){
+                gsap.to(
+                    letters[i], {
+                        duration: 5,
+                        ease: 'sine.inOut',
+                        left: '36.9%'
+                    }
+                )
+            }
+            if(i===5){
+                gsap.to(
+                    letters[i], {
+                        duration: 5,
+                        ease: 'sine.inOut',
+                        left: '42%'
+                    }
+                )
+            }
+            if(i===6){
+                gsap.to(
+                    letters[i], {
+                        duration: 5,
+                        ease: 'sine.inOut',
+                        left: '46.4%'
+                    }
+                )
+            }
+            if(i===8){
+                gsap.to(
+                    letters[i], {
+                        duration: 5,
+                        ease: 'sine.inOut',
+                        left: '53.4%'
+                    }
+                )
+            }
+            if(i===9){
+                gsap.to(
+                    letters[i], {
+                        duration: 5,
+                        ease: 'sine.inOut',
+                        left: '57%'
+                    }
+                )
+            }
+            if(i===10){
+                gsap.to(
+                    letters[i], {
+                        duration: 5,
+                        ease: 'sine.inOut',
+                        left: '61%'
+                    }
+                )
+            }
+            if(i===11){
+                gsap.to(
+                    letters[i], {
+                        duration: 5,
+                        ease: 'sine.inOut',
+                        left: '66.75%'
+                    }
+                )
+            }
+            if(i===12){
+                gsap.to(
+                    letters[i], {
+                        duration: 5,
+                        ease: 'sine.inOut',
+                        left: '70.7%'
+                    }
+                )
+            }
+            if(i===13){
+                gsap.to(
+                    letters[i], {
+                        duration: 5,
+                        ease: 'sine.inOut',
+                        left: '75.1%'
+                    }
+                )
+            }
+            if(i===14){
+                gsap.to(
+                    letters[i], {
+                        duration: 5,
+                        ease: 'sine.inOut',
+                        left: '79.8%'
+                    }
+                )
+            }
+        }
 
 
         
